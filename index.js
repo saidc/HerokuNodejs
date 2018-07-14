aa524d2f3fd606707a281a34c8212640c3881fd5
Said <saidjoc@gmail.com>
2018-07-14 08:29:40 +0000

    correccion de direccionamiento de la carpeta assets en la carpeta public y en views

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname,'client'))); //'public')));
+app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
